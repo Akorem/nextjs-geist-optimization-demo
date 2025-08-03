944dd5c8beb372313e6b31bbf14e1e8eab4fe65b
Gift <99963625+Akorem@users.noreply.github.com>
2025-08-03 11:03:23 +0100

    Add .next/static/chunks/src_app_products_[productId]_page_tsx_15665860._.js 1754215401304

---
+++ .next/static/chunks/src_app_products_[productId]_page_tsx_15665860._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_010288ae._.js",
+  "static/chunks/src_17693172._.js"
+],
+    source: "dynamic"
+});
